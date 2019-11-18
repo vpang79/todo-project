2816cffcef61b3a125b7887995fad88c2c61782d
Vincent Pang <vpang23@gmail.com>
2019-11-18 06:08:30 +0000

    refactor code and paloma attempt

--- Gemfile
+++ Gemfile
@@ -65,3 +65,4 @@ group :development, :test do
 end
 
 gem 'jquery-rails'
+gem 'paloma', '~> 5.1.0'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,4 +14,5 @@
 //= require activestorage
 //= require jquery
 //= require jquery_ujs
+//= require paloma
 //= require_tree .
